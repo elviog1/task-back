be9e8dd47b12e00c934c212279d8140ab13d43e9
Elvio Galeano <elvio.galeano827@gmail.com>
2023-07-24 13:43:57 -0300

    add

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
+  app.setGlobalPrefix('api') // esto es para que todas las rutas tengan el string API, ej: http://localhost:3000/api/task
   await app.listen(3000);
 }
 bootstrap();
